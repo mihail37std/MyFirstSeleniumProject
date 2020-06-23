aca01d233daa3a53e11a1c60db981ba58b3ef757
Vacar Mihail <mihail37std@gmail.com>
2020-06-23 21:13:51 +0300

    First try on selenium

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -13,11 +13,6 @@ public class Main {
         login.invalidPasswordLogin();
         login.invalidEmailLogin();
 
-
-
-
-
-
     }
 
 
--- src/test/java/WishListTest.java
+++ src/test/java/WishListTest.java
@@ -1,7 +1,9 @@
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class WishListTest {
     public static void main(String[] args) {
@@ -10,7 +12,7 @@ public class WishListTest {
         addItemToWishListAndChangeQuantity ();
         addItemToWishListWithoutLogin ();
         removeItemFromWishList();
-        
+        addFromWishListToCart ();
     }
 
     public static void addItemToWishList(){
@@ -80,6 +82,28 @@ public class WishListTest {
 
         Alert alert = driver.switchTo().alert();
         alert.accept();
+        driver.quit();
+    }
+
+    public static void addFromWishListToCart () {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://testfasttrackit.info/selenium-test/");
+
+        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
+        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
+        driver.findElement(By.cssSelector("#email")).sendKeys("cosmin@fasttrackit.org");
+        driver.findElement(By.cssSelector("#pass")).sendKeys("123456");
+        driver.findElement(By.cssSelector("#send2 > span > span")).click();
+
+        Actions action = new Actions(driver);
+        WebElement elementHomeDeco = driver.findElement(By.xpath("//*[@id=\"nav\"]/ol/li[4]/a"));
+        action.moveToElement(elementHomeDeco).perform();
+        driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-4.parent > ul > li.level1.nav-4-2 > a")).click();
+        driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col3-layout > div" +
+                " > div.col-wrapper > div.col-main > div.category-products > ul > li:nth-child(1) > div >" +
+                " div.actions > ul > li:nth-child(1) > a")).click();
+        driver.findElement(By.cssSelector("#wishlist-view-form > div > div > button.button.btn-add > span > span")).click();
         driver.quit();
     }
 }
