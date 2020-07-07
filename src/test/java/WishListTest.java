f6e26fcd241afca98b3d81587883e49f0f3f399f
Vacar Mihail <mihail37std@gmail.com>
2020-07-07 23:15:41 +0300

    Lab17part2

--- src/test/java/WishListTest.java
+++ src/test/java/WishListTest.java
@@ -20,7 +20,6 @@ public class WishListTest {
 
     @Test
     public void addItemToWishList() {
-        // done
         driver.findElement(By.cssSelector(".skip-account .label")).click();
         driver.findElement(By.cssSelector("a[href*='login']")).click();
         driver.findElement(By.cssSelector("#email")).sendKeys("cj01nfs@gmail.com");
@@ -38,7 +37,6 @@ public class WishListTest {
 
     @Test
     public void addItemToWishListAndChangeQuantity() {
-        //done
         driver.findElement(By.cssSelector(".skip-account .label")).click();
         driver.findElement(By.cssSelector("a[href*='login']")).click();
         driver.findElement(By.cssSelector("#email")).sendKeys("cj01nfs@gmail.com");
@@ -63,7 +61,6 @@ public class WishListTest {
 
     @Test
     public void addItemToWishListWithoutLogin() {
-        //done
         driver.findElement(By.cssSelector(".nav-5")).click();
         driver.findElement(By.cssSelector(" li:nth-child(1) div div.actions ul li:nth-child(1) a")).click();
         WebElement loginButton = driver.findElement(By.cssSelector("#send2"));
@@ -74,7 +71,6 @@ public class WishListTest {
 
     @Test
     public void removeItemFromWishList() {
-        //done
         driver.findElement(By.cssSelector(".skip-account .label")).click();
         driver.findElement(By.cssSelector("a[href*='login']")).click();
         driver.findElement(By.cssSelector("#email")).sendKeys("cj01nfs@gmail.com");
@@ -98,29 +94,27 @@ public class WishListTest {
 
     @Test
     public void addFromWishListToCart() {
-        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
-        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
+        driver.findElement(By.cssSelector(".skip-account .label")).click();
+        driver.findElement(By.cssSelector("a[href*='login']")).click();
         driver.findElement(By.cssSelector("#email")).sendKeys("cj01nfs@gmail.com");
         driver.findElement(By.cssSelector("#pass")).sendKeys("123456");
-        driver.findElement(By.cssSelector("#send2 > span > span")).click();
-        WebElement welcomeMessage = driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col2-left-layout > div > div.col-main > div.my-account > div > div.welcome-msg > p.hello > strong"));
+        driver.findElement(By.cssSelector("#send2")).click();
+        WebElement welcomeMessage = driver.findElement(By.cssSelector(".hello strong"));
         Assert.assertTrue(welcomeMessage.isDisplayed());
         Assert.assertEquals("Hello, Mihai NFS !",welcomeMessage.getText());
         Actions action = new Actions(driver);
-        WebElement elementHomeDeco = driver.findElement(By.xpath("//*[@id=\"nav\"]/ol/li[4]/a"));
+        WebElement elementHomeDeco = driver.findElement(By.cssSelector("li.nav-4"));
         action.moveToElement(elementHomeDeco).perform();
-        driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-4.parent > ul > li.level1.nav-4-2 > a")).click();
-        driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col3-layout > div" +
-                " > div.col-wrapper > div.col-main > div.category-products > ul > li:nth-child(1) > div >" +
-                " div.actions > ul > li:nth-child(1) > a")).click();
-        WebElement wishlist = driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col2-left-layout > div > div.col-main > div.my-account > div.my-wishlist > ul > li > ul > li > span"));
+        driver.findElement(By.cssSelector("li .level1.nav-4-2.menu-active")).click();
+        driver.findElement(By.cssSelector("li a[href*='wishlist/index/add/product/378']")).click();
+        WebElement wishlist = driver.findElement(By.cssSelector(".success-msg span"));
         Assert.assertTrue(wishlist.isDisplayed());
         Assert.assertEquals("Body Wash with Lemon Flower Extract and Aloe Vera has been added to your wishlist. Click here to continue shopping.",wishlist.getText());
-        driver.findElement(By.cssSelector("#wishlist-view-form > div > div > button.button.btn-add > span > span")).click();
-        WebElement shoppingCart = driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col1-layout > div > div > div.cart.display-single-price > div.page-title.title-buttons > h1"));
+        driver.findElement(By.cssSelector("[onclick='addAllWItemsToCart()']")).click();
+        WebElement shoppingCart = driver.findElement(By.cssSelector("h1"));
         Assert.assertTrue(shoppingCart.isDisplayed());
         Assert.assertEquals("SHOPPING CART",shoppingCart.getText());
-        WebElement productAddedToShoppCart = driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col1-layout > div > div > div.cart.display-single-price > ul > li > ul > li > span"));
+        WebElement productAddedToShoppCart = driver.findElement(By.cssSelector(".success-msg span"));
         Assert.assertTrue(productAddedToShoppCart.isDisplayed());
         Assert.assertEquals("1 product(s) have been added to shopping cart: \"Body Wash with Lemon Flower Extract and Aloe Vera\".",productAddedToShoppCart.getText());
     }
